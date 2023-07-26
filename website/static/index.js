3b47f63add33942e4a9fe78816f4ec4e941eea9a
Victor Jimenez <victor.jimenez07@myhunter.cuny.edu>
2023-07-26 15:33:31 -0400

    finished base website

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+//fetch to send post requests in java
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
